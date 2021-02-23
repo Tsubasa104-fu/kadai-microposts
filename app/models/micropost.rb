c02ca01c45cbe60e47ae458d7139517760103e86
Tsubasa <aile104fujikawa@gmail.com>
2021-02-23 20:16:14 +0000

    kadai-microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  has_many :favorites
+  has_many :favorites, foreign_key: "favorite_id", dependent: :destroy
 
   validates :content, presence: true, length: { maximum: 255 }
 end
